8b43e885a92b1c755b10620b2d28c8b611eb7de8
Nguyen Phu Hao <126962580+Junkohoutarou@users.noreply.github.com>
2023-12-15 13:02:04 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import streamlit as st
 from PIL import Image
 import pickle as pkl
 
-class_list = {'0': 'Negative', '1': 'Other', '2': 'Positive'}
+class_list = {'0': 'Negative', '1': 'Neutral', '2': 'Positive'}
 with open("styles.css") as f:
     custom_css = f.read()
 st.markdown(f"<style>{custom_css}</style>", unsafe_allow_html=True)
@@ -11,25 +11,19 @@ st.title('Sentiment analysis from Vietnamese students’ feedback')
 image = Image.open('vsfc.jpg')
 st.image(image)
 
-input_md = open('models.pkl', 'rb')
+input_ec = open('ec_model.pkl', 'rb')
+encoder = pkl.load(input_ec)
+
+input_md = open('model.pkl', 'rb')
 model = pkl.load(input_md)
 
-if hasattr(model, 'predict'):
-    st.header('Write a feedback')
-    txt = st.text_area('', '')
+st.header('Write a feedback')
+txt = st.text_area('', '')
 
-    if txt != '':
-        if st.button('Predict'):
-            try:
-                # Thực hiện dự đoán
-                prediction = model.predict([txt])
+if txt != '':
+    if st.button('Predict'):
+        feature_vector = encoder.transform([txt])
+        label = str((model.predict(feature_vector))[0])
 
-                # Chuyển đổi kết quả thành string để tránh lỗi Attribute Error
-                label = str(prediction[0])
-
-                st.header('Result')
-                st.text(class_list[label])
-            except Exception as e:
-                st.write(f"Error during prediction: {e}")
-else:
-    st.write("Model object does not have 'predict' method.")
+        st.header('Result')
+        st.text(class_list[label])
