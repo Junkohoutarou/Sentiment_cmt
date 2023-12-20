6e71d9de4e920bb17b44e0a26f4f18dd5ed01273
Nguyen Phu Hao <126962580+Junkohoutarou@users.noreply.github.com>
2023-12-20 11:47:32 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,42 @@
 import streamlit as st
 from PIL import Image
 import pickle as pkl
+import numpy as np
 
 class_list = {'0': 'Negative', '1': 'Neutral', '2': 'Positive'}
-with open("styles.css") as f:
-    custom_css = f.read()
-st.markdown(f"<style>{custom_css}</style>", unsafe_allow_html=True)
-st.title('Sentiment analysis from Vietnamese students’ feedback')
+st.title("Sentiment analysis from Vietnamese students’ feedback")
 
 image = Image.open('vsfc.jpg')
 st.image(image)
 
-input_ec = open('ec_model.pkl', 'rb')
-encoder = pkl.load(input_ec)
+# Đảm bảo file ec_model.pkl tồn tại
+try:
+    with open('ec_model.pkl', 'rb') as input_ec:
+        encoder = pkl.load(input_ec)
+except FileNotFoundError:
+    st.error("Error: 'ec_model.pkl' not found. Please check the file path.")
+    st.stop()
+except Exception as e:
+    st.error(f"An error occurred while loading 'ec_model.pkl': {e}")
+    st.stop()
 
-input_md = open('model.pkl', 'rb')
-model = pkl.load(input_md)
+# Đảm bảo file model.pkl tồn tại
+try:
+    with open('model.pkl', 'rb') as input_md:
+        model = pkl.load(input_md)
+except FileNotFoundError:
+    st.error("Error: 'model.pkl' not found. Please check the file path.")
+    st.stop()
+except Exception as e:
+    st.error(f"An error occurred while loading 'model.pkl': {e}")
+    st.stop()
 
 st.header('Write a feedback')
 txt = st.text_area('', '')
 
-prediction_result = model.predict(feature_vector)
-print(prediction_result)
+if txt != '':
+    feature_vector = encoder.transform([txt])
+    prediction_result = model.predict(feature_vector)
+
+    st.header('Result')
+    st.text(class_list[str(prediction_result[0])])
