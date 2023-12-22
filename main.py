97303ad965477213a79c0ef97b1b86c2614cb53d
Nguyen Phu Hao <126962580+Junkohoutarou@users.noreply.github.com>
2023-12-22 23:15:44 +0700

    Update main.py

--- main.py
+++ main.py
@@ -3,5 +3,11 @@ import streamlit as st
 from PIL import Image
 import pickle as pkl
 import numpy as np
+class_list = {'0': 'Negative', '1': 'Neutral', '2': 'Positive'}
+st.title("Sentiment analysis from Vietnamese students’ feedback")
+
+image = Image.open('vsfc.jpg')
+st.image(image)
+
 
 
