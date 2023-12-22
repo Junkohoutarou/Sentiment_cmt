819bf43d875dfa9000a479567636771ca7378849
Nguyen Phu Hao <126962580+Junkohoutarou@users.noreply.github.com>
2023-12-22 23:16:04 +0700

    Update main.py

--- main.py
+++ main.py
@@ -19,3 +19,25 @@ except Exception as e:
     st.error(f"An error occurred while loading 'ec_model.pkl': {e}")
     st.stop()
 # Đảm bảo file model.pkl tồn tại
+try:
+    with open('model.pkl', 'rb') as input_md:
+        model = pkl.load(input_md)
+except FileNotFoundError:
+    st.error("Error: 'model.pkl' not found. Please check the file path.")
+    st.stop()
+except Exception as e:
+    st.error(f"An error occurred while loading 'model.pkl': {e}")
+    st.stop()
+
+st.header('Write a feedback')
+txt = st.text_area('', '')
+
+if txt != '':
+    feature_vector = encoder.transform([txt])
+    prediction_result = model.predict(feature_vector)
+
+    st.header('Result')
+    st.text(class_list[str(prediction_result[0])])
+
+
+
