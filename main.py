c4f71a306bc3afd71618553753a280d9d8634471
Nguyen Phu Hao <126962580+Junkohoutarou@users.noreply.github.com>
2023-12-23 23:16:43 +0700

    Update main.py

--- main.py
+++ main.py
@@ -4,4 +4,6 @@ from PIL import Image
 import pickle as pkl
 import numpy as np
 
+class_list = {'0': 'Negative', '1': 'Neutral', '2': 'Positive'}
+st.title("Sentiment analysis from Vietnamese students’ feedback")
 
