8fc58b53217a81fc4b831d31768a3072912a1aad
Nguyen Phu Hao <126962580+Junkohoutarou@users.noreply.github.com>
2023-12-23 23:17:33 +0700

    Update main.py

--- main.py
+++ main.py
@@ -11,3 +11,16 @@ st.title("Sentiment analysis from Vietnamese students’ feedback")
 image = Image.open('vsfc.jpg')
 st.image(image)
 
+
+image = Image.open('vsfc.jpg')
+st.image(image)
+# Đảm bảo file ec_model.pkl tồn tại
+try:
+    with open('ec_model.pkl', 'rb') as input_ec:
+        encoder = pkl.load(input_ec)
+except FileNotFoundError:
+    st.error("Error: 'ec_model.pkl' not found. Please check the file path.")
+    st.stop()
+except Exception as e:
+    st.error(f"An error occurred while loading 'ec_model.pkl': {e}")
+    st.stop()
