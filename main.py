515c3fd4b122196b66d939fc67e0de8211db0143
Nguyen Phu Hao <126962580+Junkohoutarou@users.noreply.github.com>
2023-12-23 23:16:58 +0700

    Update main.py

--- main.py
+++ main.py
@@ -7,3 +7,7 @@ import numpy as np
 class_list = {'0': 'Negative', '1': 'Neutral', '2': 'Positive'}
 st.title("Sentiment analysis from Vietnamese students’ feedback")
 
+
+image = Image.open('vsfc.jpg')
+st.image(image)
+x
